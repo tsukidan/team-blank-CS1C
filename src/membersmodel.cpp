a0768f68a745a43917ac2825c592d53cafcb8d81
ocboogie <boogie@mikulec.com>
2020-04-22 15:50:30 -0700

    Show revenue from member

--- src/membersmodel.cpp
+++ src/membersmodel.cpp
@@ -1,17 +1,42 @@
 #include "membersmodel.h"
+#include <QDebug>
 
 /**
  * @brief MembersModel::MembersModel
  * @param parent
  */
 MembersModel::MembersModel(QObject *parent) : QSqlTableModel(parent) {
+  QSqlQuery query;
+  query.prepare("SELECT "
+                "    members.id,"
+                "    SUM(purchases.quantity * items.price) AS revenue "
+                "FROM purchases "
+                "INNER JOIN members ON members.id = purchases.member_id "
+                "INNER JOIN items ON items.id = purchases.item_id "
+                "GROUP BY members.id");
+  if (!query.exec()) {
+    qDebug() << "Failed to search for quantity sold and revenue: "
+             << query.lastError().text();
+    return;
+  }
+
+  while (query.next()) {
+    int id = query.value(0).toInt();
+    int revenueFromMember = query.value(1).toInt();
+    if (revenueFromMember != 0) {
+      revenue.insert(id, revenueFromMember);
+    }
+  }
+
   setTable("members");
   select();
+
   setHeaderData(0, Qt::Horizontal, tr("Membership number"));
   setHeaderData(1, Qt::Horizontal, tr("Name"));
   setHeaderData(2, Qt::Horizontal, tr("Type"));
   setHeaderData(3, Qt::Horizontal, tr("Expiration"));
   setHeaderData(4, Qt::Horizontal, tr("Rebate"));
+  setHeaderData(5, Qt::Horizontal, tr("Revenue"));
 }
 
 /**
@@ -23,6 +48,17 @@ void MembersModel::filterByMonth(QDate date) {
   setFilter(QString("expiration = '%1'").arg(dateString));
 }
 
+Qt::ItemFlags MembersModel::flags(const QModelIndex &index) const {
+  if (index.column() == 5) {
+    return Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemNeverHasChildren;
+  }
+  return QSqlTableModel::flags(index);
+}
+
+int MembersModel::columnCount(const QModelIndex &parent) const {
+  return QSqlTableModel::columnCount(parent) + 1;
+}
+
 QVariant MembersModel::data(const QModelIndex &index, int role) const {
   if (role == Qt::DisplayRole) {
     switch (index.column()) {
@@ -30,6 +66,10 @@ QVariant MembersModel::data(const QModelIndex &index, int role) const {
       return QSqlTableModel::data(index).toBool() ? "Executive" : "Regular";
     case 4:
       return Utils::moneyDisplay(QSqlTableModel::data(index).toInt());
+    case 5:
+      int id = QSqlTableModel::record(index.row()).value(0).toInt();
+
+      return Utils::moneyDisplay(revenue[id]);
     }
   }
   return QSqlTableModel::data(index, role);
--- src/membersmodel.h
+++ src/membersmodel.h
@@ -5,17 +5,23 @@
 #include <QDate>
 #include <QLocale>
 #include <QSqlTableModel>
+#include <QtSql>
 
 class MembersModel : public QSqlTableModel {
 public:
   MembersModel(QObject *parent = nullptr);
 
+  virtual Qt::ItemFlags flags(const QModelIndex &index) const override;
   virtual QVariant data(const QModelIndex &index,
                         int role = Qt::DisplayRole) const override;
+  virtual int columnCount(const QModelIndex &index) const override;
 
   void filterByMonth(QDate date);
 
   void clearFilterByMonth();
+
+private:
+  QHash<int, int> revenue;
 };
 
 #endif // MEMBERSMODEL_H
