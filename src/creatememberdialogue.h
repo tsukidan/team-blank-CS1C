72ced5b4e955e9e1e88c7632975d920bf45ec3df
ocboogie <boogie@mikulec.com>
2020-05-11 14:43:41 -0700

    Format create member dialogue

--- src/creatememberdialogue.h
+++ src/creatememberdialogue.h
@@ -1,29 +1,28 @@
 #ifndef CREATEMEMBERDIALOGUE_H
 #define CREATEMEMBERDIALOGUE_H
 
-#include <QDialog>
 #include "member.h"
+#include <QDialog>
 
 namespace Ui {
 class createMemberdialogue;
 }
 
-class createMemberdialogue : public QDialog
-{
-    Q_OBJECT
+class createMemberdialogue : public QDialog {
+  Q_OBJECT
 
 public:
-    explicit createMemberdialogue(QWidget *parent = nullptr);
-    ~createMemberdialogue();
-    Member getMember();
+  explicit createMemberdialogue(QWidget *parent = nullptr);
+  ~createMemberdialogue();
+  Member getMember();
 
 private slots:
 
-    void on_buttonBox_accepted();
+  void on_buttonBox_accepted();
 
 private:
-    Ui::createMemberdialogue *ui;
-    Member member;
+  Ui::createMemberdialogue *ui;
+  Member member;
 };
 
 #endif // CREATEMEMBERDIALOGUE_H
