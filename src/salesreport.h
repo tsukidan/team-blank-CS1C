cf7943c44637368b083a6f5d48b05b6a9fc9a4d9
ocboogie <boogie@mikulec.com>
2020-05-11 05:55:14 -0700

    Format sales report

--- src/salesreport.h
+++ src/salesreport.h
@@ -4,33 +4,31 @@
 #include "utils.h"
 #include <QDate>
 #include <QLocale>
-#include <QWidget>
-#include <QSqlQueryModel>
 #include <QSql>
+#include <QSqlQueryModel>
+#include <QWidget>
 #include <QtSql>
 
 namespace Ui {
 class Salesreport;
 }
-
-class Salesreport : public QWidget
-{
-    Q_OBJECT
+class Salesreport : public QWidget {
+  Q_OBJECT
 
 public:
-    explicit Salesreport(QWidget *parent = nullptr);
-    ~Salesreport();
+  explicit Salesreport(QWidget *parent = nullptr);
+  ~Salesreport();
 
 private slots:
-    void on_filterType_stateChanged(int arg1);
+  void on_filterType_stateChanged(int arg1);
 
-    void on_dateEdit_dateChanged(const QDate &date);
+  void on_dateEdit_dateChanged(const QDate &date);
 
-    void on_memberType_currentIndexChanged(int index);
+  void on_memberType_currentIndexChanged(int index);
 
 private:
-    void updateMembers();
-    Ui::Salesreport *ui;
+  void updateMembers();
+  Ui::Salesreport *ui;
 };
 
 #endif // SALESREPORT_H
